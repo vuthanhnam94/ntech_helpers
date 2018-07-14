2cb4b5f338292f4a211413508360f2e1c97270ce
namtv <vuthanhnam94@gmail.com>
2018-07-14 18:08:41 +0700

    Define NTechDevelpers.Utilities.Assembly project

---
+++ implementation/NTechDevelpers.Utilities.Assembly/AssemblyUtils.cs
@@ -0,0 +1,111 @@
+﻿namespace NTechDevelpers.Utilities.Assembly
+{
+    using Common.Logging;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    /// <summary>
+    /// The AssemblyUtils class
+    /// </summary>
+    public static class AssemblyUtils
+    {
+        /// <summary>
+        /// The logger instance
+        /// </summary>
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(AssemblyUtils));
+
+        /// <summary>
+        /// Define assembly pattern to scan
+        /// </summary>
+        public static Func<string, bool> AssemblyPatternFunc = s => !string.IsNullOrEmpty(s) && s.StartsWith(VersionScanningPattern);
+
+        /// <summary>
+        /// Version scanning pattern
+        /// </summary>
+        private const string VersionScanningPattern = "NTechDevelopers.";
+
+        /// <summary>
+        /// Getfolders the version.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        /// <returns>Version of folder</returns>
+        public static string GetfolderVersion(string filePath)
+        {
+            var extension = new List<string>() { "*.dll", "*.exe" };
+            var version = string.Empty;
+            var listVersion = new List<Version>();
+            foreach (var item in extension)
+            {
+                FileUtils.SafeAccess(
+                    () =>
+                    {
+                        var files = new DirectoryInfo(filePath).GetFiles(item, SearchOption.AllDirectories);
+                        foreach (var file in files)
+                        {
+                            var name = file.FullName;
+                            var versionStr = FileVersionInfo.GetVersionInfo(name).FileVersion;
+                            if (!string.IsNullOrEmpty(versionStr))
+                            {
+                                var versionInfo = new Version(versionStr);
+                                listVersion.Add(versionInfo);
+                            }
+                        }
+                    },
+                    filePath);
+            }
+
+            if (listVersion.Count > 0)
+            {
+                version = listVersion.Max().ToString();
+            }
+
+            return version;
+        }
+
+        /// <summary>
+        /// Get version of file
+        /// </summary>
+        /// <param name="filePath">path of directory</param>
+        /// <returns>System.String.</returns>
+        public static string GetMaxFileVersion(string filePath)
+        {
+            var extension = new List<string>() { "*.dll", "*.exe" };
+            var version = string.Empty;
+            var listVersion = new List<Version>();
+            var assemblyVersion = new Version();
+            foreach (var item in extension)
+            {
+                FileUtils.SafeAccess(
+                    () =>
+                    {
+                        var files = new DirectoryInfo(filePath).GetFiles(item, SearchOption.AllDirectories);
+                        foreach (var file in files)
+                        {
+                            var name = file.FullName;
+                            if (file.Name.StartsWith(VersionScanningPattern))
+                            {
+                                var versionStr = FileVersionInfo.GetVersionInfo(name).FileVersion;
+                                if (!string.IsNullOrEmpty(versionStr))
+                                {
+                                    var versionInfo = new Version(versionStr);
+                                    listVersion.Add(versionInfo);
+                                }
+                            }
+                        }
+                    },
+                    filePath);
+            }
+
+            if (listVersion.Count > 0)
+            {
+                version = listVersion.Max().ToString();
+            }
+
+            return version;
+        }
+
+    }
+}
---
+++ implementation/NTechDevelpers.Utilities.Assembly/FileUtils.cs
@@ -0,0 +1,65 @@
+﻿namespace NTechDevelpers.Utilities.Assembly
+{
+    using Common.Logging;
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// The FileUtils class.
+    /// </summary>
+    public static class FileUtils
+    {
+        /// <summary>
+        /// The logger instance
+        /// </summary>
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(FileUtils));
+
+        /// <summary>
+        /// Execute read & write IO files
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="filePath"></param>
+        public static void SafeAccess(Action action, string filePath)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                var message = $"Access to the path '{filePath}' is denied";
+                Logger.Error(message, ex);
+                new Exception(message);
+            }
+            catch (PathTooLongException ex)
+            {
+                var message = $"Path or file [{filePath}] is longer than supported";
+                Logger.Error(message, ex);
+                new Exception(message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                var message = $"Part of a file or directory [{filePath}] cannot be found";
+                Logger.Error(message, ex);
+                new Exception(message);
+            }
+            catch (FileNotFoundException)
+            {
+                var message = $"Path or file [{filePath}] does not exist";
+                Logger.Error(message);
+                new Exception(message);
+            }
+            catch (IOException ex)
+            {
+                var message = $"General IO exception [{ex.Message}]";
+                Logger.Error(message, ex);
+                throw new Exception(message);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message, ex);
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
